591833fb9ee85b70960829c60969a0a5f830dda9
mohamaddmortada <mortada_mohamad@hotmail.com>
2024-12-04 20:39:17 +0200

    adding some mock data

---
+++ src/data/data.js
@@ -0,0 +1,26 @@
+const Data = [
+    {
+      title: "Players",
+      questions: [
+          {
+            type: "mcq",
+            question: "Who has won the most Ballon d'Or awards as of 2024?",
+            options: ["Lionel Messi", "Cristiano Ronaldo", "Zinedine Zidane", "Ronaldinho"],
+            answer: "Lionel Messi",
+          },
+          {
+            type: "mcq",
+            question: "Who won the champions league 3 times with 3 different clubs",
+            options: ["Lionel Messi", "Cristiano Ronaldo", "Zinedine Zidane", "Sedorf"],
+            answer: "Sedorf",
+          },
+          {
+            type: "input",
+            question: "Which country is Pelé from?",
+            answer: "Brazil",
+          },
+      ],
+    },
+]
+
+export default Data;
