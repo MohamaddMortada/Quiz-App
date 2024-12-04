d3790d16b8c2b38ae6077f3212ae8d8967be40ac
mohamaddmortada <mortada_mohamad@hotmail.com>
2024-12-04 20:45:26 +0200

    adding SelectedQuiz.jsx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
+import SelectedQuiz from './components/SelectedQuiz';
+import Data from './data/data';
+const App = () => {
+  const [Quiz, setQuiz] = useState(null);
 
-function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Quiz App</h1>
+        <SelectedQuiz Data={Data} setQuiz={setQuiz} />
     </div>
   );
-}
-
+};
 export default App;
---
+++ src/components/SelectedQuiz.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const SelectedQuiz = ({ Data, setQuiz }) => {
+  return (
+    <div>
+      <h2>Select a Quiz</h2>
+      {Data.map((quiz) => (
+        <button key={quiz.id} onClick={() => setQuiz(quiz)}>
+          {quiz.title}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default SelectedQuiz;
